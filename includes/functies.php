52494a3984d6d5496dd6096cb9186974bc0bbaa8
saebuabu <a.saebu@kw1c.nl>
2015-04-19 18:26:49 +0200

    default aanroep

--- includes/functies.php
+++ includes/functies.php
@@ -1,7 +1,7 @@
      
 <?php
     
-function printMenu($currentPagina) {
+function printMenu($currentPagina='Home') {
 echo <<<MENU
    <ul class="vert-one">
           <li><a href="#" title="Portfolio menu" class="current">Home</a></li>
